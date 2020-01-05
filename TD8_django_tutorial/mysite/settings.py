37a2eba8f778aaae796c6873fdf5f5bbff96edb2
Luc Bertin <lucbertin@outlook.com>
2020-01-06 00:23:22 +0100

    added model House

--- TD8_django_tutorial/mysite/settings.py
+++ TD8_django_tutorial/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'predicteur_app.apps.PredicteurAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris' # was UTC
 
 USE_I18N = True
 
--- TD8_django_tutorial/predicteur_app/models.py
+++ TD8_django_tutorial/predicteur_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class House(models.Model):
+    CRIM        = models.FloatField()
+    ZN          = models.FloatField()
+    INDUS       = models.FloatField()
+    CHAS        = models.FloatField()
+    NOX         = models.FloatField()
+    RM          = models.FloatField()
+    AGE         = models.FloatField()
+    DIS         = models.FloatField()
+    RAD         = models.FloatField()
+    TAX         = models.FloatField()
+    PTRATIO     = models.FloatField()
+    B           = models.FloatField()
+    LSTAT       = models.FloatField()
+    # The dependent variable: y
+    MEDV          = models.FloatField(null=True)
+    # Just to give a date to the creation of the object instance
+    created       = models.DateTimeField(auto_now_add=True)
+
+    # We don't have to...
+    #class Meta:
+    #    ordering = ['created']
